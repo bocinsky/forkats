96e8f7e7591fe510ceed55ab432f914826aa7edc
R. Kyle Bocinsky <bocinsky@gmail.com>
2019-02-22 23:00:01 -0700

    added extraction function

--- R/tct_tree.R
+++ R/tct_tree.R
@@ -121,9 +121,7 @@ tct_levels <- function(x)
   x
 }
 
-#' @describeIn tct_tree The basic extraction function for a `tct_tree`.
 #' @export
-# define [ method
 `[.tct_tree` <- function(x, i, j, ..., drop = TRUE) {
   cls <- class(x)
   atrb <- attributes(x)
